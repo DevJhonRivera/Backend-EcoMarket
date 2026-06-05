817927635e81e7b8558a9ac4582ddc2d6684a42c
JhonRivera00 <riverajhon052@gmail.com>
2026-06-04 23:48:25 -0500

    Add Clg

--- src/db.js
+++ src/db.js
@@ -4,6 +4,7 @@ import { MONGO_URL } from "./config.js";
 (async()=>{
     try {
         mongoose.set("strictQuery", false);
+        console.log("Mongo URL",MONGO_URL)
         const db = await mongoose.connect(MONGO_URL)
         console.log(`Conectado a ${db.connection.name}`)
     } catch (error) {
