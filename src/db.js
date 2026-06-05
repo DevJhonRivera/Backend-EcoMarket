abc426081d2a0c25adcb5efc08713f376cb4467c
JhonRivera00 <riverajhon052@gmail.com>
2026-06-05 01:08:31 -0500

    XXXXXXX

--- src/db.js
+++ src/db.js
@@ -1,27 +1,12 @@
-import mongoose from 'mongoose';
-import { MONGO_URL } from './config.js';
+import mongoose from "mongoose"
+import { MONGO_URL } from "./config.js";
 
-console.log("MONGO_URL existe:", !!MONGO_URL);
-
-(async () => {
-  try {
-
-    mongoose.set("strictQuery", false);
-
-    console.log("Intentando conectar a Mongo...");
-
-    const db = await mongoose.connect(MONGO_URL);
-
-    console.log(
-      `BASE DE DATOS CONECTADA ${db.connection.name}`
-    );
-
-  } catch (error) {
-
-    console.error(
-      "ERROR CONEXION MONGO:",
-      error
-    );
-
-  }
-})();
+(async()=>{
+    try {
+        mongoose.set("strictQuery", false);
+        const db = await mongoose.connect(MONGO_URL)
+        console.log(`Conectado a ${db.connection.name}`)
+    } catch (error) {
+        console.log(error)  
+    }
+})()
