f3f5f7f1a8c87b070ee83cb4bb7f59c3e0fb09ab
JhonRivera00 <riverajhon052@gmail.com>
2026-06-07 10:02:36 -0500

    add .

--- src/db.js
+++ src/db.js
@@ -1,14 +1,34 @@
 import mongoose from "mongoose"
 import { MONGO_URL } from "./config.js";
 
+      // Eventos de monitoreo
+    mongoose.connection.on("connected", () => {
+      console.log("✅ MongoDB conectado");
+    });
+
+    mongoose.connection.on("disconnected", () => {
+      console.log("❌ MongoDB desconectado");
+    });
+
+    mongoose.connection.on("error", (err) => {
+      console.error("🔥 Error MongoDB:", err);
+    });
 
 // Conexión a la base de datos
-(async()=>{
-    try {
-        mongoose.set("strictQuery", false);
-        const db = await mongoose.connect(MONGO_URL)
-        console.log(`Conectado a ${db.connection.name}`)
-    } catch (error) {
-        console.log(error)  
-    }
-})()
+(async () => {
+  try {
+    mongoose.set("strictQuery", false);
+
+    const db = await mongoose.connect(MONGO_URL,{
+        serverSelectionTimeoutMS: 5000,
+        socketTimeoutMS:45000
+    });
+    console.log(`Conectado a ${db.connection.name}`);
+
+
+  } catch (error) {
+    console.error("Error MongoDB:");
+    console.error(error.message);
+    console.error(error);
+  }
+})();
